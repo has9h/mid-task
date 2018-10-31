a02b2dc08883ce5b197790a9997d92d669341dad
Hasnain Hossain <has9h2@gmail.com>
2018-10-31 03:18:40 +0000

    Basic layout

---
+++ script.js
@@ -0,0 +1,32 @@
+.div1 {
+    width: 100px;
+    height: 50px;
+    border: 1px solid blue;
+    box-sizing: border-box;
+    padding-top: 14px;
+    text-align: center;
+}
+
+.div2 {
+    width: 100px;
+    height: 50px;    
+    padding-top: 50px;
+    border: 1px solid red;
+    box-sizing: border-box;
+    padding-top: 14px;
+    text-align: center;
+    float:left;
+}
+
+.flex-container {
+  display: flex;
+  background-color: DodgerBlue;
+  flex-wrap: wrap;
+}
+
+.flex-container > div {
+  background-color: #006666;
+  margin: 15px;
+  padding: 25px;
+  font-size: 16px;
+}
